4b2f5b8df34de00145eb55132caef27fbe4687d9
HarshadRoundtech <harshadj@roundtechsquare.com>
2023-04-28 20:53:49 +0530

    changed auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,36 +2,54 @@ require('dotenv').config();
 const CreateError = require('http-errors');
 const axios = require('axios');
 const INTEGRATION_KEY = process.env.INTEGRATION_KEY;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const SECRET_KEY = process.env.SECRET_KEY;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
 
 
-const getdocusigncode = async () => {
-    try {
-        return new Promise(async (resolve, reject) => {
-            const response = await axios({
-                method: 'post',
-                url: 'https://account-d.docusign.com/oauth/auth',
-                params: {
-                    response_type: 'code',
-                    scope: 'signature',
-                    client_id: INTEGRATION_KEY,
-                    redirect_uri: REDIRECT_URI,
-                }
-            });
-            resolve(response.data);
-        });
-    } catch (error) {
-        reject(error);
-     }
-};
+// const getdocusigncode = async () => {
+//     try {
+//         return new Promise(async (resolve, reject) => {
+//             const response = await axios({
+//                 method: 'post',
+//                 url: 'https://account-d.docusign.com/oauth/auth',
+//                 params: {
+//                     response_type: 'code',
+//                     scope: 'signature',
+//                     client_id: INTEGRATION_KEY,
+//                     redirect_uri: REDIRECT_URI,
+//                 }
+//             });
+//             resolve(response.data);
+//         });
+//     } catch (error) {
+//         reject(error);
+//      }
+// };
 const getdocusignaccesstoken = async (req, res, next) => {
     try {
-        const code = await getdocusigncode();
-        console.log(code);
+        const code = req.query.code;
+        const response = await axios({
+            method: 'post',
+            url: 'https://account-d.docusign.com/oauth/token',
+            data: {
+                grant_type: 'authorization_code',
+                code: code,
+            },
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: INTEGRATION_KEY,
+                password: SECRET_KEY,
+            },
+        });
+        res.status(200).json({
+            message: 'Access Token Generated',
+            data: response.data,
+        });
     } catch (error) {
-        next(error);
+        next(error)
     }
 };
 
